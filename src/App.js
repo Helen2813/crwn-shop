162757337d1c2b53d77e8c5c811a4c2f7d100981
Helen2813 <e.abalenkova@gmail.com>
2020-11-03 21:06:42 +0300

    added routs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = props => (
+  <div>
+    <h1>HATS: {props.match.params.topicId}</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topics' component={HatsPage} />
+        <Route path='/topics/:topicId' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
