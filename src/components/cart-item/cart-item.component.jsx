398c6a42f9fa7dd3dd91adec50a2eb3d7d44411d
Elena Krikun <ekrikun@voiso.com>
2020-12-13 15:48:30 +0300

    added checkout-item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { toggleCartHidden } from '../../redux/cart/cart.actions';
-// import  { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-item.style.scss';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
